6c15c162c004c894728206983044e27a166ef0d8
vuquochuy188 <83956104+vuquochuy188@users.noreply.github.com>
2021-07-01 23:44:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,23 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    introTitle: 'Hi Linh thảo mai!',
+    introDesc: `Ɲgười ơi hãу tin anh nàу
+Và em đến đâу đi
+Ɲgàn hoa lá đang thaу màu
+Ɲàу em thích hoa gì
+Ɲụ cười bé xinh xuân ngời
+Ɲgười làm xao xuуến tim anh
+Tình anh sẽ không phai mờ
+Vì anh đã trao dần`,
+    btnIntro: 'xiaxia',
+    title: 'Phải chăng Linh đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng Linh đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích Huy <33',
+    btnNo: 'Ko, điêu quen :)',
+    question:'Trên thế giới hơn 7 tỉ người mà sao c lại yêu t <3',
+    btnReply: 'Gửi cho Huy nhé <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu :v.',
+    mess: 'T biết mà 🥰. Yêu c nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 23h t qua đón nhé đi lượn nhé :v. Giờ thì quay lại thôi',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/messages/t/100018623115795' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
